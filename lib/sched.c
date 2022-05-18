ed712b7ae4a09a79624b51caaad0a20e6f0afd0b
20373091 <20373091@buaa.edu.cn>
2022-05-18 13:05:16 +0800

    de sched

--- lib/sched.c
+++ lib/sched.c
@@ -12,12 +12,12 @@
  *  3. CANNOT use `return` statement!
  */
 /*** exercise 3.15 ***/
-struct Env *e = NULL;
+//struct Env *e = NULL;
 void sched_yield(void)
 {
     static int count = 0; // remaining time slices of current env
     static int point = 0; // current env_sched_list index
-//	static struct Env *e;
+	static struct Env *e = NULL;
     /*  hint:
      *  1. if (count==0), insert `e` into `env_sched_list[1-point]`
      *     using LIST_REMOVE and LIST_INSERT_TAIL.*/
@@ -30,6 +30,7 @@ void sched_yield(void)
      *  functions or macros below may be used (not all):
      *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
      */
+	/*
 	e = curenv;
 	if(count == 0 || e == NULL || e->env_status == ENV_NOT_RUNNABLE ){
 		do{
@@ -51,7 +52,27 @@ void sched_yield(void)
 				point = 1- point;
 			}	
 		} while(e == NULL || e->env_status != ENV_RUNNABLE);
+	}*/
+	if(count == 0 || e == NULL || e->env_status != ENV_RUNNABLE){
+		if(e != NULL){
+			LIST_REMOVE(e, env_sched_link);
+			LIST_INSERT_TAIL(&env_sched_list[1-point],e , env_sched_link);
+		}
+		while(1){
+			while(LIST_EMPTY(&env_sched_list[point])) point = 1-point;
+			e = LIST_FIRST(&env_sched_list[point]);
+			if(e->env_status == ENV_RUNNABLE){
+				count = e->env_pri;
+				break;
+			}else{
+				if(e->env_status == ENV_NOT_RUNNABLE){
+					LIST_REMOVE(e, env_sched_link);
+					LIST_INSERT_TAIL(&env_sched_list[1-point], e, env_sched_link);
+				}
+			}
+		}
 	}
+
 	count --;
 	
 	env_run(e);
